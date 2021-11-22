5712974e1bf293d6a411710aca7c62edee40e0e5
alejandro <alejandroweil13@gmail.com>
2021-11-21 21:59:44 -0500

    Work on first page nearly done, 3 inputs created with labels

--- src/components/firstPage/firstPage.jsx
+++ src/components/firstPage/firstPage.jsx
@@ -5,6 +5,7 @@ class FirstPage extends React.Component{
   constructor(props){
     super(props);
     this.state = this.props.initialState
+    this.update = this.update.bind(this);
   }
 
   update(field) {
@@ -12,9 +13,40 @@ class FirstPage extends React.Component{
   }
 
   render(){
+    console.log(this.state)
     return(
       <div className='form-background'>
-        <p className="intro">Whats Up Bitch</p>
+        <h1 className='introduction-blurb'>
+          Alejandro's Coterie Insurance Take Home Project
+        </h1>
+        <h2 className='information-request'>
+          Please fill in your business details below!
+        </h2>
+        <form className="initial-form">
+          <div className='label-name'>
+            What's your Business Name?
+            <input
+              className="business-name-input"
+              name='business-name' 
+              type="text" 
+              placeholder="Business Name" 
+              onChange={this.update("businessName")}
+            />
+          </div>
+          <div className="industry-selector"> Industry
+            <select onChange={this.update("industryId")} className='industry-selector'>
+              <option value="10537">Plumber</option>
+              <option value="10391">Software developer</option>
+              <option value="10415">Lawyer</option>
+              <option value="10109">Handyman</option>
+            </select>
+          </div>
+          <div className='email-input-container'>
+            Email
+            <input className='email-input' type="email" />
+          </div>
+          
+        </form>
       </div>
     )
   }
